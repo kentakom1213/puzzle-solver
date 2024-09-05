978098255e82e73919d04017e192207b8253dbb4
kentakom1213 <kentakom1213@gmail.com>
2024-09-05 23:39:18 +0900

    [add] 数独ソルバを追加

---
+++ sudoku/src/main.rs
@@ -0,0 +1,18 @@
+use colored::Colorize;
+use sudoku::sudoku::Sudoku;
+
+fn main() {
+    // 入力受け取り
+    let mut sudoku = Sudoku::from_stdin();
+
+    // 盤面の表示
+    println!("\n{:^37}", "Problem".red().bold());
+    sudoku.show();
+
+    // 解を求める
+    sudoku.solve();
+
+    // 解の表示
+    println!("\n{:^37}", "Answer".green().bold());
+    sudoku.show();
+}
